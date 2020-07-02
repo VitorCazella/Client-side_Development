4196e4811bb867f548c080c62ee13b7f0171381c
VitorCazella <cazella1998>
2020-07-02 12:42:48 +1200

    Navbar complete

--- Project/nz_pics/src/js/App.jsx
+++ Project/nz_pics/src/js/App.jsx
@@ -1,33 +1,47 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Main from './pages/Main.jsx';
 import Albuns from './pages/Albuns.jsx';
 import NewPhotos from './pages/NewPhotos.jsx';
 import '../css/App.css';
-import { Component } from 'react';
-// import './debug.css'
+// import '../css/debug.css'
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      page: <Main />,
+      page: <Albuns />
     }
   }
 
   setSelected(e) {
     this.setState({ page: e });
+
+
+    var btnContainer = document.getElementById("nav");
+
+    // Get all buttons with class="btn" inside the container
+    var btns = btnContainer.querySelectorAll('li a');
+
+    // Loop through the buttons and add the active class to the current/clicked button
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function () {
+        var current = btnContainer.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+      });
+    }
   }
 
   render() {
     return (
       <div className="App">
         <section className="sidebarLeft">
-          <ul>
-            <li><a href="#" onClick={() => this.setSelected(<Main />)}>Home</a></li>
-            <li><a href="#" onClick={() => this.setSelected(<Albuns />)}>Albums</a></li>
-            <li><a href="#" onClick={() => this.setSelected(<NewPhotos />)}>New Photos</a></li>
-            <li><a href="#" onClick={() => this.setSelected(<Main />)}>Photography</a></li>
-            <li><a href="#" onClick={() => this.setSelected(<Main />)}>About</a></li>
+          <ul id="nav">
+            <li><a className="btn active" href="#" onClick={() => this.setSelected(<Main />)}>Home</a></li>
+            <li><a className="btn" href="#" onClick={() => this.setSelected(<Albuns />)}>Albums</a></li>
+            <li><a className="btn" href="#" onClick={() => this.setSelected(<NewPhotos />)}>New Photos</a></li>
+            <li><a className="btn" href="#" onClick={() => this.setSelected(<Main />)}>Photography</a></li>
+            <li><a className="btn" href="#" onClick={() => this.setSelected(<Main />)}>About</a></li>
           </ul>
         </section>
         <div className="main">{this.state.page}</div>
--- Project/nz_pics/src/js/pages/Albuns.jsx
+++ Project/nz_pics/src/js/pages/Albuns.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import { Container, Card, Button, Row, Col } from 'react-bootstrap';
 
 function Albuns() {
-    return(
-        <div>ALBUNS</div>
+    return (
+        <div>
+            <Container fluid="md">
+                <Row>
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
 
